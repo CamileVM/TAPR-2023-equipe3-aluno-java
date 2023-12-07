1ea6cbb334ca7c15fa8552784a02f4986b6dd869
CamileVM <111375529+CamileVM@users.noreply.github.com>
2023-12-07 20:45:43 -0300

    Create Disciplina.java

---
+++ microservprofessor/src/main/java/br/edu/univille/microservprofessor/Disciplina.java
@@ -0,0 +1,44 @@
+package br.edu.univille.microservprofessor.entity;
+
+import org.springfrapmework.data.annotation.Id;
+
+import com.azure.spring.data.cosmos.core.mapping.Container;
+import com.azure.spring.data.cosmos.core.mapping.GeneratedValue;
+import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
+
+@Container(containerName = "dados",autoCreateContainer = true)
+public class Dados {
+    @Id
+    @PartitionKey
+    @GeneratedValue
+    public String id;
+    public String nome;
+    public String endereco;
+    public String telefone;
+    
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+}
